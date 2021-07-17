44478cc189e75b05b9d19075950dd236fbdd0838
Nivethithaa M <nivethithaaathithan75@gmail.com>
2021-07-17 17:49:51 +0530

    Build to deploy

---
+++ build/precache-manifest.982ef11159f1f7a835eefe820a6e556a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d26dce1f63fc7c46fa856b9e9ee7d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3518b1de77e4c9a5fd75",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "42fd8a3ec1839e4238a3",
+    "url": "/static/css/main.906d9089.chunk.css"
+  },
+  {
+    "revision": "3518b1de77e4c9a5fd75",
+    "url": "/static/js/2.ed2e1252.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.ed2e1252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fd8a3ec1839e4238a3",
+    "url": "/static/js/main.64d789db.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
