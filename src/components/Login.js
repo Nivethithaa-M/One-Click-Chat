1a5fb9e14e731fc8a3ca75853123e89b32b49659
Nivethithaa M <nivethithaaathithan75@gmail.com>
2021-07-17 18:09:35 +0530

    Updating Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleOutlined , FacebookOutlined} from "@ant-design/icons";
+import {GoogleOutlined } from "@ant-design/icons";
 import "firebase/app"
 import { auth } from '../firebase';
 import firebase from 'firebase/app';
@@ -13,12 +13,12 @@ const Login = () => {
                 onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                     <GoogleOutlined/> SignIn with Google
                 </div>
-                <br/><br/>
+                {/* <br/><br/>
                 <div 
                 className="login-button facebook"
                 onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
                     <FacebookOutlined/> SignIn with Facebook
-                </div>
+                </div> */}
             </div>
         </div>
     )
